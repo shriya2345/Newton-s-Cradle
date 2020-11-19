a04a4dd8c3549fbfd876e302c26f31772f925fd3
shriya2345 <73680842+shriya2345@users.noreply.github.com>
2020-11-19 14:59:13 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -12,7 +12,7 @@ this.x=x;
 this.y=y;
 this.r=r;
 
-this.body=Bodies.circle(this.x,this.y,this.r,options);
+this.body=Bodies.circle(this.x,this.y,this.r/2,options);
 World.add(world,this.body);
 
  }
